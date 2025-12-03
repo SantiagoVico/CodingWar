7287ee0d23dd33f2fc8af909d010c3aa36444c14
Jaynos6k <santiago.vico@e-artsup.net>
2025-12-03 13:02:47 +0100

    Credit card mask cleared

---
+++ CreditCardMask.cs
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5412509bd436bd33920011bc/train/csharp
+public static class CreditCardMask
+{
+    public static string Maskify(string cc)
+    {
+        if (cc.Length <= 4) return cc;
+        return new string('#', cc.Length - 4) + cc.Substring(cc.Length - 4);
+    }
+}
