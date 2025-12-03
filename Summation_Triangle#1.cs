bb0086148bad22e34631e714fb4b4624ff42e1aa
Jaynos6k <santiago.vico@e-artsup.net>
2025-12-03 11:56:53 +0100

    removed main and unsed using directive

--- Summation_Triangle#1.cs
+++ Summation_Triangle#1.cs
@@ -1,18 +1,10 @@
 // https://www.codewars.com/kata/6357825a00fba284e0189798/train/csharp
-
-using System;
-
 public class SummationTriangle
 {
     public static long GetSum(int n)
     {
         long m = (long)n + 1;
+        // closed form: sum_{i=0..n} (i+1)(2i+1) = m(m+1)(4m-1)/6 with m = n+1
         return m * (m + 1) * (4 * m - 1) / 6;
-    }   
-
-    static void Main(string[] args)
-    {
-        Console.WriteLine(GetSum(2)); // 22
-        Console.WriteLine(GetSum(3)); // 50
     }
 }
